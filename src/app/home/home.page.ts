7384f95c7bf899b221d74d0090376debc5d64354
PhilippeDi4s <125489606+PhilippeDi4s@users.noreply.github.com>
2024-09-30 11:45:40 +0000

    Julia&Philippe

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { AlertController, IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { NumberFormatStyle } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
   constructor() {}
+
+  public altura: number | undefined;
+  public peso: number | undefined;
+  public valorImc: number | undefined;
+  public classificacao: string="";
+
+  calcularImc (){
+    if(this.altura && this.peso){
+      this.valorImc = (this.peso/(this.altura*this.altura));
+
+      if(this.valorImc< 18.5 ){
+        this.classificacao='Abaixo do peso';
+      }else
+
+      if(this.valorImc >= 18.5 && this.valorImc < 25 ){
+        this.classificacao='Peso normal';
+      }else
+
+      if(this.valorImc >= 25 && this.valorImc < 30 ){
+        this.classificacao='Sobrepeso';
+      }else
+
+      if(this.valorImc >= 30){
+        this.classificacao='Obesidade';
+      }
+
+    }
+  }
 }
+
